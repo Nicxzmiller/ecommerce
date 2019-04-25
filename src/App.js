067085413fcf848e2714d29b9ff317367207cd9f
nicxzmiller <nicxzmiller2@gmail.com>
2019-04-25 17:46:00 +0200

    navbar creation and logo setting

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Switch,Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -7,15 +8,19 @@ import ProductList from './components/ProductList';
 import Details from './components/Details';
 import Cart from './components/Cart';
 import Default from './components/Default';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <React.Fragment>
       <Navbar />
-      <ProductList />
-      <Details />
-      <Cart />
-      <Default />
+      <Switch>
+        <Route exact path="/" component={ProductList}></Route>
+        <Route path="/details" component={Details}></Route>
+        <Route path="/cart" component={Cart}></Route>
+        <Route path="/contact" component={Contact}></Route>
+        <Route component={Default}></Route>
+      </Switch>
     </React.Fragment>
   );
 }
