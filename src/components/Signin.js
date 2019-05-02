cf240efed6aa0b68ab604702ca977c5cc6fb1c71
nicxzmiller <nicxzmiller2@gmail.com>
2019-05-02 14:35:58 +0200

    CSS tweaking of products page

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -8,28 +8,30 @@ class Signin extends Component {
     render() {
         return (
             <Router>
-                <div className="Appp">
-                    <div className="App__Aside"></div>
-                        <div className="App__Form">
-                            <div className="PageSwitcher">
-                                <NavLink to="/sign-in" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">SIGN IN</NavLink>
-                                <NavLink exact to="/" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">SIGN UP</NavLink>
+                <div className="card-body">
+                    <div className="Appp">
+                        <div className="App__Aside"></div>
+                            <div className="App__Form">
+                                <div className="PageSwitcher">
+                                    <NavLink to="/sign-in" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">SIGN IN</NavLink>
+                                    <NavLink exact to="/" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">SIGN UP</NavLink>
+                                </div>
+
+                                <div className="FormTitle">
+                                    <NavLink to="/sign-in" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">SIGN IN</NavLink> or
+                                     <NavLink exact to="/" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">SIGN UP</NavLink>
+                                </div>
+
+                                <Route exact path="/" component={SignUpForm}>
+
+                                </Route>
+
+                                <Route path="/sign-in" component={SignInForm}>
+
+                                </Route>
                             </div>
 
-                            <div className="FormTitle">
-                                <NavLink to="/sign-in" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">SIGN IN</NavLink> or
-                                 <NavLink exact to="/" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">SIGN UP</NavLink>
-                            </div>
-
-                            <Route exact path="/" component={SignUpForm}>
-
-                            </Route>
-
-                            <Route path="/sign-in" component={SignInForm}>
-
-                            </Route>
-                        </div>
-
+                    </div>
                 </div>
             </Router>
         );
